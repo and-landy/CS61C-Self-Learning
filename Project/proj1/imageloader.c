60e69e6d8cefa3cda43fbfa47cb897300651f8b2
and-landy <landy.zhong1004@gmail.com>
2026-03-20 22:37:09 -0400

    Commit part A

--- Project/proj1/imageloader.c
+++ Project/proj1/imageloader.c
@@ -7,7 +7,7 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **              Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				Yueyang Zhong
 **
 **
 ** DATE:        2020-08-15
@@ -23,19 +23,71 @@
 //Opens a .ppm P3 image file, and constructs an Image object. 
 //You may find the function fscanf useful.
 //Make sure that you close the file with fclose before returning.
-Image *readData(char *filename) 
+Image *readData(char *filename)
 {
-	//YOUR CODE HERE
+	FILE *fp = fopen(filename, "r");
+
+	// Read the PPM header: format (P3), width, height, and max color value
+	char format[3];
+	int width, height, maxval;
+	fscanf(fp, "%s", format);
+	fscanf(fp, "%d %d", &width, &height);
+	fscanf(fp, "%d", &maxval);
+
+	// Allocate the Image struct and set dimensions
+	Image *img = malloc(sizeof(Image));
+	img -> rows = height;
+	img -> cols = width;
+
+	// Allocate a 2D array of Color pixels (array of row pointers, each pointing to an array of Colors)
+	img->image = malloc(height * sizeof(Color *));
+	for(int i = 0; i < height; i++){
+		img->image[i] = malloc(width * sizeof(Color));
+	}
+
+	// Read each pixel's RGB values from the file into the 2D array
+	for(int i = 0; i < height; i++){
+		for(int j = 0; j < width; j++){
+			int r, g, b;
+			fscanf(fp, "%d %d %d", &r, &g, &b);
+			img->image[i][j].R = r;
+			img->image[i][j].G = g;
+			img->image[i][j].B = b;
+		}
+	}
+
+	fclose(fp);
+	return img;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	printf("P3\n");
+	printf("%d %d\n", image->cols, image->rows);
+	printf("255\n");
+
+	for(int i = 0; i < image->rows; i++){
+		for(int j = 0; j < image->cols; j++){
+			printf("%3d ", image->image[i][j].R);
+			printf("%3d ", image->image[i][j].G);
+			printf("%3d", image->image[i][j].B);
+			if(j != image->cols - 1){
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+	for(int i = 0; i < image->rows; i++){
+		free(image->image[i]);
+	}
+
+	free(image->image);
+
+	free(image);
 }
--- Project/proj1/steganography.c
+++ Project/proj1/steganography.c
@@ -7,12 +7,13 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **				Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				Yueyang Zhong
 **
 ** DATE:        2020-08-23
 **
 **************************************************************************/
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
@@ -21,13 +22,48 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+	//Allocate space for new collor
+	Color *c = malloc(sizeof(Color));
+
+	//Read each RGB's color from 2D array to collor
+	c->R = image->image[row][col].R;
+	c->G = image->image[row][col].G;
+	c->B = image->image[row][col].B;
+
+	return c;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+	//Allocate image struc and copy the dimension of input image
+	Image *n = malloc(sizeof(Image));
+	n->rows = image->rows;
+	n->cols = image->cols;
+
+	n->image = malloc(image->rows * sizeof(Color *));
+	for(int i = 0; i < image->rows; i++){
+		n->image[i] = malloc(image->cols * sizeof(Color));
+	}
+
+	//Determine the last bit of B and assign correct value for RGB
+	for(int i = 0; i < image->rows; i++){
+		for(int j = 0; j < image->cols; j++){
+			int lastBit = image->image[i][j].B & 1;
+			if(lastBit == 0){
+				n->image[i][j].R = 0;
+				n->image[i][j].G = 0;
+				n->image[i][j].B = 0;
+			}
+			else{
+				n->image[i][j].R = 255;
+				n->image[i][j].G = 255;
+				n->image[i][j].B = 255;
+			}
+		}
+	}
+
+	return n;
 }
 
 /*
@@ -45,5 +81,20 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+	if(argc != 2){
+		return -1;
+	}
+
+	int len = strlen(argv[1]);
+	if(!(argv[1][len - 4] == '.' && argv[1][len - 3] == 'p' && argv[1][len - 2] == 'p' && argv[1][len - 1] == 'm')){
+		return -1;
+	}
+
+	Image *img = readData(argv[1]);
+	Image *newimg = steganography(img);
+	writeData(newimg);
+	freeImage(img);
+	freeImage(newimg);
+
+	return 0;
 }
