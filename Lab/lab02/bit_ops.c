49914bcb2381c0605a73194faf31c7996e7993ac
and-landy <landy.zhong1004@gmail.com>
2026-03-27 21:49:52 -0400

    Complete lab02 bit ops and vector exercises

--- Lab/lab02/bit_ops.c
+++ Lab/lab02/bit_ops.c
@@ -5,23 +5,29 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
-    // 0 or 1)
-    return -1;
+    x = x << (31 - n);
+    x = x >> 31;
+    return x;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    unsigned shift = 1 << n;
+    if(v == 0){
+        shift = ~shift;
+        *x = *x & shift;
+    }
+    else{
+        *x = *x | shift;
+    }
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    unsigned shift = 1 << n;
+    *x = *x ^ shift;
 }
 
--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -5,6 +5,24 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    unsigned a0 = 1 & *reg;
+    unsigned a2 = 1 & (*reg >> 2);
+    unsigned a3 = 1 & (*reg >> 3);
+    unsigned a5 = 1 & (*reg >> 5);
+
+    unsigned r = a0 ^ a2;
+    r = r ^ a3;
+    r = r ^ a5;
+
+    *reg = *reg >> 1;
+
+    unsigned shift = 1 << 15;
+    if(r == 0){
+        shift = ~shift;
+        *reg = *reg & shift;
+    }
+    else{
+        *reg = *reg | shift;
+    }
 }
 
